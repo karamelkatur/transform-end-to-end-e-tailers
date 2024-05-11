aafe9bd724ed90b82b3d563118aab4b2ac3621e9
Karamelka <156432536+karamelkatur@users.noreply.github.com>
2024-05-11 06:02:17 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,14 @@
-def generate_random_data():
-import random
-
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    return data
-    main()
-def main():
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-if __name__ == "__main__":
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    return data
-def main():
-
-
-    for item in data:
-
-    main()
-        print(f"Random Number: {item}")
 
     data = generate_random_data()
+
+        print(f"Random Number: {item}")
+def main():
+import random
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+
+if __name__ == "__main__":
+def generate_random_data():
+    for item in data:
